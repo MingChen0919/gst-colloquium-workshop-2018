478fc9f8814e22035b2c5704e2298d59c68283f5
Ming Chen <ming.chen0919@gmail.com>
2018-03-06 12:42:15 -0500

    finish pca

--- misc/datasets.R
+++ misc/datasets.R
@@ -1,6 +1,7 @@
-library(stringr)
+
 
 #-------- tutorial 1 dataset----------
+library(stringr)
 traj_0_dat = 'https://raw.githubusercontent.com/MingChen0919/gst-colloquium-workshop-2018/master/content/datasets/John_datasets/traj_0.dat'
 feature_names = str_split(readLines(traj_0_dat)[1], '--')[[1]]
 
@@ -19,13 +20,19 @@ energy_tics = read.table(energy_tics_url, header = TRUE, stringsAsFactors = FALS
 energy_tics$frame_index = 1:nrow(energy_tics)
 energy_tics = energy_tics[, c(6, 1:5)]
 
-microstate_trajectory_url = 'https://raw.githubusercontent.com/MingChen0919/gst-colloquium-workshop-2018/master/content/datasets/John_datasets/traj_0.microclusters.dat'
-microstate_trajectory = t(read.table(microstate_trajectory_url, header = FALSE, stringsAsFactors = FALSE))
-macrostate_trajectory_url = 'https://raw.githubusercontent.com/MingChen0919/gst-colloquium-workshop-2018/master/content/datasets/John_datasets/traj_0.macroclusters.dat'
-macrostate_trajectory = t(read.table(macrostate_trajectory_url, header = FALSE, stringsAsFactors = FALSE))
+macrostate = vector(mode = 'integer')
+macrostate[which((energy_tics$TIC0 < 3) & (energy_tics$TIC1 > -2))] = 1
+macrostate[which((energy_tics$TIC0 < 3) & (energy_tics$TIC1 > -4.5) & (energy_tics$TIC1 <= -2) )] = 2
+macrostate[which((energy_tics$TIC0 < 3) & (energy_tics$TIC1 <= -4.5))] = 3
+macrostate[which(energy_tics$TIC0 >= 3)] = 4
+energy_tics$macrostate = macrostate
 
-energy_tics$microstate = microstate_trajectory
-energy_tics$macrostate = macrostate_trajectory
+# microstate_trajectory_url = 'https://raw.githubusercontent.com/MingChen0919/gst-colloquium-workshop-2018/master/content/datasets/John_datasets/traj_0.microclusters.dat'
+# microstate_trajectory = t(read.table(microstate_trajectory_url, header = FALSE, stringsAsFactors = FALSE))
+# macrostate_trajectory_url = 'https://raw.githubusercontent.com/MingChen0919/gst-colloquium-workshop-2018/master/content/datasets/John_datasets/traj_0.macroclusters.dat'
+# macrostate_trajectory = t(read.table(macrostate_trajectory_url, header = FALSE, stringsAsFactors = FALSE))
+# energy_tics$microstate = microstate_trajectory
+
 
 # save data into .RData
 save(trajectory, energy_tics, file = 'content/datasets/datasets.RData')
